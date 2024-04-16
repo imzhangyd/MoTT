9d0be392cf429d1ec081014c31532c67db7ff07e
imzhangyd <ydzhangyd@163.com>
2024-04-16 08:30:16 +0800

    update

--- tracking.py
+++ tracking.py
@@ -83,7 +83,7 @@ def parse_args_():
     parser.add_argument(
         "--model_ckpt_path",
         type=str,
-        default="./checkpoint/20240412_15_51_17_tracks10_ckpt/20240412_16_05_35.chkpt",
+        default="./checkpoint/20240415_14_18_06_tracks10_del_existflag_ckpt/20240415_14_31_00.chkpt",
     )
 
     parser.add_argument("--holdnum", type=int, default=10)
