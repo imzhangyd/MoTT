7352ea2fbca71314398f20e948a46c9f863a76db
imzhangyd <ydzhangyd@163.com>
2024-04-19 10:48:41 +0800

    update

--- transformer/Models_v23.py
+++ transformer/Models_v23.py
@@ -125,6 +125,73 @@ class Encoder(nn.Module):
         return (enc_output,)
 
 
+class Encoder_2(nn.Module):
+    """A encoder model with self attention mechanism."""
+
+    def __init__(
+        self,
+        d_word_vec,
+        n_layers,
+        n_head,
+        d_k,
+        d_v,
+        d_model,
+        d_inner,
+        dropout=0.1,
+        n_position=200,
+        scale_emb=True,
+        n_length=7,
+        inoutdim=3,
+    ):
+
+        super().__init__()
+
+        self.src_word_emb = nn.Linear(inoutdim * n_length, d_model)
+        self.position_enc = PositionalEncoding(d_word_vec, n_position=n_position)
+        self.dropout = nn.Dropout(p=dropout)
+        self.layer_stack = nn.ModuleList(
+            [
+                EncoderLayer(
+                    d_model,
+                    d_inner,
+                    n_head,
+                    d_k,
+                    d_v,
+                    n_length=n_length,
+                    dropout=dropout,
+                )
+                for _ in range(n_layers)
+            ]
+        )
+        self.layer_norm = nn.LayerNorm([d_model], eps=1e-6)
+
+        self.scale_emb = scale_emb
+        self.d_model = d_model
+
+    def forward(self, src_seq, src_mask, return_attns=False):
+
+        enc_slf_attn_list = []
+        src_seq = src_seq.view(
+            list(src_seq.size())[:-2] + [-1]
+        )  # [bs, num_cand, len_future*featnum]
+        # -- Forward
+        enc_output = self.src_word_emb(
+            src_seq
+        )  # change all_zero item [bs, length-1, d_model]
+        if self.scale_emb:
+            enc_output *= self.d_model**0.5  # amplification
+        enc_output = self.dropout(self.position_enc(enc_output, passed=True))
+        # enc_output = self.layer_norm(enc_output)
+
+        for enc_layer in self.layer_stack:
+            enc_output, enc_slf_attn = enc_layer(enc_output, slf_attn_mask=src_mask)
+            enc_slf_attn_list += [enc_slf_attn] if return_attns else []
+        enc_output = self.layer_norm(enc_output)
+        if return_attns:
+            return enc_output, enc_slf_attn_list
+        return (enc_output,)
+
+
 class Decoder(nn.Module):
     """A decoder model with self attention mechanism."""
 
@@ -270,12 +337,7 @@ class Decoder_1(nn.Module):
 
 
 class Pred_clshead(nn.Module):
-    def __init__(
-        self,
-        d_model,
-        dropout=0.1,
-        inoutdim=3,
-    ):
+    def __init__(self, d_model, dropout=0.1, inoutdim=3, cand_num=25):
         super(Pred_clshead, self).__init__()
         # self.reg_mlp = nn.Sequential(
         #     nn.Linear(d_model, d_model, bias=True),
@@ -291,24 +353,16 @@ class Pred_clshead(nn.Module):
             nn.Dropout(p=dropout),
             nn.Linear(d_model * 2, d_model, bias=True),
             nn.Linear(d_model, d_model // 2, bias=True),
-            nn.Linear(d_model // 2, 1, bias=True),
+            nn.Linear(d_model // 2, cand_num, bias=True),
         )
 
         self.cls_opt = nn.Softmax(dim=-1)
 
         self.inoutdim = inoutdim
 
-    def forward(self, x):  # bs,candi_num=25, d_model
-        # pred = self.reg_mlp(x)  # bs,candi_num=25, inoutdim*n_length
-        # pred = pred.transpose(-1, -2)  # bs, inoutdim*n_length,candi_num=25
-        # pred = self.reg_linear(pred).squeeze(dim=-1)  # bs, inoutdim*n_length
-        # pred = pred.view(*pred.shape[0:1], -1, self.inoutdim)  # bs, n_length, inoutdim
-        # inoutdim = normed[s_x, s_y, s_size, s_inten,  x, y, size, inten,   abs shiftx, abs shift y, abs dist,     flag]
-        # pred[..., -1] = torch.sigmoid(pred[..., -1])
-        # pred_pos = pred[:,:,:-1].cumsum(dim=-2)
-        # pred = torch.cat([pred_pos,pred[:,:,-1:]],-1)
+    def forward(self, x):  # bs,globaltoken=1, d_model
 
-        cls_h = self.cls_FFN(x).squeeze(dim=-1)
+        cls_h = self.cls_FFN(x).squeeze(dim=-2)
         conf = self.cls_opt(cls_h)
         return cls_h
 
@@ -340,7 +394,6 @@ class Pred_reghead(nn.Module):
         pred[..., -1] = torch.sigmoid(pred[..., -1])
         # pred_pos = pred[:,:,:-1].cumsum(dim=-2)
         # pred = torch.cat([pred_pos,pred[:,:,-1:]],-1)
-
         # cls_h = self.cls_FFN(x).squeeze(dim=-1)
         # conf = self.cls_opt(cls_h)
         return pred  # , cls_h
@@ -386,6 +439,20 @@ class Transformer(nn.Module):
             inoutdim=inoutdim,
         )
 
+        self.encoder_2 = Encoder_2(
+            n_position=n_position,
+            d_word_vec=d_word_vec,
+            d_model=d_model,
+            d_inner=d_inner,
+            n_layers=n_layers,
+            n_head=n_head,
+            d_k=d_k,
+            d_v=d_v,
+            dropout=dropout,
+            scale_emb=scale_emb,
+            n_length=n_future,
+            inoutdim=inoutdim,
+        )
         self.decoder_1 = Decoder_1(
             n_position=n_position,
             d_word_vec=d_word_vec,
@@ -402,31 +469,12 @@ class Transformer(nn.Module):
         )
 
         self.vit_token = nn.Parameter(torch.randn(1, n_future, inoutdim))
-        self.decoder_2 = Decoder(
-            n_position=n_position,
-            d_word_vec=d_word_vec,
-            d_model=d_model,
-            d_inner=d_inner,
-            n_layers=n_layers,
-            n_head=n_head,
-            d_k=d_k,
-            d_v=d_v,
-            dropout=dropout,
-            scale_emb=scale_emb,
-            n_length=n_future,
-            inoutdim=inoutdim,
-        )
 
         self.pred_clshead = Pred_clshead(
-            d_model=self.d_model,
-            dropout=dropout,
-            inoutdim=inoutdim,
+            d_model=self.d_model, dropout=dropout, inoutdim=inoutdim, cand_num=n_candi
         )
+        self.pred_reghead = Pred_reghead(d_model=self.d_model, inoutdim=inoutdim)
 
-        self.pred_reghead = Pred_reghead(
-            d_model=self.d_model,
-            inoutdim=inoutdim,
-        )
         self.convert_ = nn.Sequential(
             nn.Linear(self.d_model * n_future, self.d_model * n_future, bias=True),
             nn.Linear(self.d_model * n_future, d_model, bias=True),
@@ -446,10 +494,6 @@ class Transformer(nn.Module):
         enc_output, *_ = self.encoder(src_seq, None, return_attns=True)
         # enc_output [bs, len_past, dim]
         # trg_seq [bs, num_cand, len_future, dim]
-        # croatt_mask = torch.matmul(
-        #     torch.ones_like(trg_seq[:, :, 0, -1:]),
-        #     src_seq[:, :, -1:].transpose(-2, -1),
-        # )
 
         future_tokens = repeat(
             self.vit_token, "a b d -> (m a) b d", m=src_seq.size()[0]
@@ -464,18 +508,11 @@ class Transformer(nn.Module):
         )
 
         pred_shift = self.pred_reghead(dec_output)
+        trg_seq = torch.cat((pred_shift.unsqueeze(1), trg_seq), dim=1)
 
-        dec_output = dec_output.view(dec_output.size()[0], 1, -1)
-        dec_output = self.convert_(dec_output)
+        enc_output, *_ = self.encoder_2(trg_seq, None, return_attns=True)
+        # enc_output  [bs, len_cand+1, dim]
 
-        dec_output, *_ = self.decoder_2(
-            trg_seq=trg_seq,
-            trg_mask=None,
-            enc_output=dec_output,
-            src_mask=None,
-            return_attns=True,
-        )
-
-        pred_score = self.pred_clshead(dec_output)
+        pred_score = self.pred_clshead(enc_output[:, :1])
 
         return pred_shift, pred_score
